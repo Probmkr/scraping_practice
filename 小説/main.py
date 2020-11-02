318858c650c2b5378f50b23abb7d0d500981b655
Thanatos9911 <thanatos9911@gmail.com>
2020-11-03 00:46:38 +0900

    update

---
+++ 小説/main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+def rf(num):
+    for _ in range(num):
+        print()
+
+def line(num):
+    print('-'*num)
+
+res = requests.get('https://localprod.pandateacher.com/python-manuscript/crawler-html/spider-men5.0.html')
+txt = res.text
+
+soup = BeautifulSoup(txt, "html.parser")
+item = soup.find_all(name='div', class_='books')
+print(item)
+rf(19)
+line(70)
+rf(3)
+for i in item:
+    print(i)
+    rf(3)
+    line(70)
+    rf(3)
+
+
